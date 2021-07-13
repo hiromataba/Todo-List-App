1e2aeeb00b677c788035f4396ff3fc700f85de1f
Hiro Mataba <aganzemataba@gmail.com>
2021-07-13 21:16:36 +0200

    Add Listsof tasks

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const list = document.querySelector('.list');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+function displayLists(toDo) {
+  const text = `<li class="item">
+                       <i class="uil uil-square circle-icon" job="complete"></i>
+                       <p class="text" > ${toDo}<p>
+                       <i class="uil uil-trash-alt" job="delete"></i>
+               </li>`;
 
-  return element;
+  const position = 'beforeend';
+  list.insertAdjacentHTML(position, text);
 }
+const arr = [];
+let index = 0;
 
-document.body.appendChild(component());
+document.addEventListener('keyup', (event) => {
+  const input = document.querySelector('#addlist');
+  if (event.which === 13) {
+    const toDo = input.value;
+    if (toDo) {
+      displayLists(toDo, true, index);
+      arr.push({
+        name: toDo,
+        completed: true,
+        index,
+      });
+      index += 1;
+      localStorage.setItem('List', JSON.stringify(arr));
+    }
+    input.value = '';
+  }
+});
