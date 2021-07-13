0d0401a1d982d8fbd3906afe102a2348ff4db10c
Hiro Mataba <aganzemataba@gmail.com>
2021-07-14 01:18:57 +0200

    Make Required Changes

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
 import './style.css';
 
 const list = document.querySelector('.list');
+const form = document.getElementById('form');
+const arr = JSON.parse(localStorage.getItem('List')) || [];
 
-function displayLists(toDo) {
-  const text = `<li class="item">
-                       <i class="uil uil-square circle-icon" job="complete"></i>
-                       <p class="text" > ${toDo}<p>
-                       <i class="uil uil-trash-alt" job="delete"></i>
-               </li>`;
+window.addEventListener('load', () => {
+  const arr = JSON.parse(localStorage.getItem('List')) || [];
+  arr.forEach((task) => {
+    list.innerHTML += `<li class="item">
+                            <div class="check">
+                                <input type="checkbox" name="checkbox" id= ${task.index}>
+                                <label class="text" for= ${task.index}> ${task.name}<labe> 
+                            </div>
+                          <i class="uil uil-trash-alt" job="delete"></i>
+                       </li>`;
+  });
+});
 
-  const position = 'beforeend';
-  list.insertAdjacentHTML(position, text);
-}
-const arr = [];
-let index = 0;
-
-document.addEventListener('keyup', (event) => {
+form.addEventListener('submit', () => {
   const input = document.querySelector('#addlist');
-  if (event.which === 13) {
-    const toDo = input.value;
-    if (toDo) {
-      displayLists(toDo, true, index);
-      arr.push({
-        name: toDo,
-        completed: true,
-        index,
-      });
-      index += 1;
-      localStorage.setItem('List', JSON.stringify(arr));
-    }
-    input.value = '';
+
+  const toDo = input.value;
+  if (toDo) {
+    arr.push({
+      name: toDo,
+      completed: true,
+      index: new Date(),
+    });
+
+    localStorage.setItem('List', JSON.stringify(arr));
   }
+  input.value = '';
 });
